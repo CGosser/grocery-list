f9922660da2166a25f77fa00b9c5a357dd1692fc
Chris Gosser and Jack Skantel <chris@>
2018-08-13 12:02:42 -0700

    create function to capitalize words

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,12 @@
+function caps(words) {
+  var capsList = capsMe.map(function(word){
+    return word.charAt(0).toUpperCase() + word.slice(1)
+  })
+}
+
+function sorter(sortMe){
+
+}
 var alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 
 $(document).ready(function() {
