443a1119587bb4b70fa19d22401f17c8de3d6ad1
Chris Gosser and Jack Skantel <jack@>
2018-08-13 11:44:32 -0700

    add placeholder elements and make buttons functional

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,14 @@
+var alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+
 $(document).ready(function() {
-  
+  var inputList = []
+  $("#inputs").submit(function(){
+    inputList.push($("#food").val());
+    console.log(inputList);
+    event.preventDefault()
+  })
+  $("#displayList").click(function(){
+  $("#shoppingList").toggle();
+  event.preventDefault();
+})
+})
