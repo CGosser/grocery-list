d0195df370322ee15fd120bf1ef0a1f8a9a28ee1
Chris Gosser and Jack Skantel <chris@>
2018-08-13 14:37:48 -0700

    create function to alphabetize and capitalize inputs

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,34 @@
 function caps(words) {
-  var capsList = capsMe.map(function(word){
-    return word.charAt(0).toUpperCase() + word.slice(1)
-  })
+return words.map(function(word){
+
+    return word.charAt(0).toUpperCase() + word.slice(1);
+  });
 }
 
-function sorter(sortMe){
 
-}
-var alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+var cappycap = []
+
+
+// var alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 
 $(document).ready(function() {
   var inputList = []
+  var outputsList = []
   $("#inputs").submit(function(){
+    if ($("#food").val() != ""){
     inputList.push($("#food").val());
     console.log(inputList);
+  }
     event.preventDefault()
+    $("#food").val("")
   })
+
   $("#displayList").click(function(){
   $("#shoppingList").toggle();
-  event.preventDefault();
+  outputsList = caps(inputList).sort();
+  console.log(outputsList);
+  outputsList.forEach(function(buy){
+  $("#shoppingList").append("<li>" + buy + "</li>")
 })
+  })
 })
